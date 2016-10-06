6f8b2a39df62b9fef34191548e33ba0d854b84f9
gregorydhughes <gregorydhughes@gmail.com>
2016-10-06 16:43:12 -0400

    Made progress!

--- Program/Program/ExcelClass.cs
+++ Program/Program/ExcelClass.cs
@@ -1,30 +1,63 @@
 ﻿using System.Windows;
+using System.Collections.Generic;
+using System.Text;
 using nmspcExceptionClass;
+using Excel = Microsoft.Office.Interop.Excel;
 using System;
 
 namespace nmspcExcelOutputClass
 {
 	class ExcelClass
     {
-		private Vector rows;
-		private Vector cols;
+		private const string DEFAULT_WORKSHEET_NAME = "Sheet1";
 
-		private string fileName;
+		private List<string> rows;
+		private List<string> cols;
+
+		private string fn;
+
+		public string fileName
+		{
+			get
+			{
+				return fn;
+			}
+			set
+			{
+				fn = value;
+			}
+		}
+
+		private string wsn;
+		
+		public string workSheetName
+		{
+			get
+			{
+				return wsn;
+			}
+			set
+			{
+				wsn = value;
+			}
+		}
 
         public ExcelClass()
         {
-										
-        }
+			workSheetName = DEFAULT_WORKSHEET_NAME;
+		}
 
-		public ExcelClass(Vector r, Vector c)
+		public ExcelClass(List<string> c)
+		{
+			cols = c;
+			workSheetName = DEFAULT_WORKSHEET_NAME;
+		}
+
+		public ExcelClass(List<string> r, List<string> c)
 		{
 			rows = r;
 			cols = c;
-		}
-
-		public void SetFileName(string fn)
-		{
-			fileName = fn;
+			workSheetName = DEFAULT_WORKSHEET_NAME;
 		}
 		
 		public void Build()
@@ -32,7 +65,29 @@ namespace nmspcExcelOutputClass
 
 			if (fileName != null)
 			{
+				Excel.Application xlApp = new Excel.Application();
 
+				xlApp.Visible = false;
+
+				Excel.Workbook wb = xlApp.Workbooks.Add();
+
+				Excel.Worksheet ws = wb.Sheets.Add();
+
+				ws.Name = workSheetName;
+
+				int pos = 1;
+
+				foreach (string col in cols)
+				{
+					ws.Cells[1, pos] = col;
+					pos++;
+				}
+
+				wb.SaveAs(@"D:\Users\Gregory\Documents\MusicSheets\" + fileName);
+
+				wb.Close();
+
+				xlApp.Quit();
 			}
 			else
 			{
--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -4,14 +4,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using nmspcExcelOutputClass;
+using System.IO;
 
 namespace Source
 {
     class Program
     {
+		static private List<string> filesToGen;
+
+		static private List<string> columnsInXl;
+
         static void Main(string[] args)
         {
-            
+			filesToGen = new List<string>();
+
+			columnsInXl = new List<string>();
+
+			GetNamesOfFilesToGenerate();
+
+			Console.Clear();
+
+			bool unique = SetNamesOfColumnsToGenerateReturnUnique();
+
+			Console.Clear();
+
+			if (!unique)
+			{
+				Console.WriteLine("Generating non unique excel workbooks...");
+
+				string cat;
+				int loc = 1;
+
+				foreach (string file in filesToGen)
+				{
+					cat = (loc < 10) ? "0" + loc.ToString() : loc.ToString();
+
+					GenerateExcelSheet(cat+file, columnsInXl);
+
+					loc++;
+				}
+			}
+			else
+			{
+				Console.WriteLine("Not generating unique excel workbooks...");
+			}
         }
+
+		static void GetNamesOfFilesToGenerate()
+		{
+			Console.WriteLine("Please enter the name of the file with the names of the excel sheets you would like to generate:");
+
+			string fName = Console.ReadLine();
+
+			while (!File.Exists(fName))
+			{
+				Console.WriteLine("File not found, please re-enter file:");
+				fName = Console.ReadLine();
+			}
+
+			ReadFileIntoList(fName, ref filesToGen);
+		}
+
+		static bool SetNamesOfColumnsToGenerateReturnUnique()
+		{
+
+			if (GetAnswerToQuestion("Do you want to generate the same columns for all sheets?"))
+			{
+				Console.WriteLine("Please enter the name of the file with the names of the columns you would like to insert into all the sheets:");
+
+				string fName = Console.ReadLine();
+
+				while (!File.Exists(fName))
+				{
+					Console.WriteLine("File not found, please re-enter file:");
+					fName = Console.ReadLine();
+				}
+
+				ReadFileIntoList(fName, ref columnsInXl);
+
+				return false;
+			}
+			else
+			{
+				// oh bother
+				// iterate through each excel sheet and ask the names of the columns to generate for each sheet
+
+				return true;
+			}
+		}
+
+		static void ReadFileIntoList(string fName, ref List<string> l)
+		{
+			StreamReader sR = new StreamReader(fName);
+
+			string line = sR.ReadLine();
+
+			while (line != null)
+			{
+				l.Add(line);
+
+				line = sR.ReadLine();
+			}
+
+			sR.Close();
+		}
+
+		static void GenerateExcelSheet(string file, List<string> cols)
+		{
+			ExcelClass ec = new ExcelClass(cols);
+
+			ec.fileName = file;
+
+			ec.workSheetName = "Music";
+
+			ec.Build();
+		}
+
+		static bool GetAnswerToQuestion(string question)
+		{
+			Console.WriteLine(question);
+
+			char ans = Convert.ToChar(Console.ReadLine().ToLower()[0]);
+
+			switch (ans)
+			{
+				case 'y':
+					return true;
+					break;
+			}
+
+			return false;
+		}
     }
 }
